c1482bcdbe33fe9ccde6421e114ac399b1a01307
Chitra <chitralekhakundaram952@gmail.com>
2024-07-10 12:10:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+// import express from "express";
+const app = express()
+const port = process.env.PORT || 4000
+//pc has many ports eg 65kports some ports are free
+//our server listens on that port
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/twitter",(req,res)=>{
+    res.send("chitra95")
+})
+
+app.get("/login",(req,res)=>{
+    res.send("<h1>Please login </h1>")
+})
+
+app.get("/youtube",(req,res)=>{
+    res.send("<h2>CHAI AUR CODE</h2>")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
